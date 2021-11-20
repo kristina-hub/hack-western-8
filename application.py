ed3451c01179736f4d14e39765ebfa366e522ae6
Kristina Kacmarova <kkacmaro@uwo.ca>
2021-11-19 21:19:06 -0500

    python file

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+application = Flask(__name__)
+@application.route('/')
+
+def index():
+    return render_template('index.html')
+    # return 'Hello World'
+
+# export FLASK_APP="application.py"
+# flask run
