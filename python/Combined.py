bffe17b6f8707d8e53e028ce2ce3e82bd3850848
Alexandra Drazilov <alexdrazilov@gmail.com>
2021-11-20 20:57:23 -0500

    Confirmed that companies are not added twice

--- python/Combined.py
+++ python/Combined.py
@@ -1,5 +1,6 @@
 import requests
 import spacy
+from difflib import SequenceMatcher
 from bs4 import BeautifulSoup
 from googlesearch import search
 import yfinance as yf
@@ -20,6 +21,11 @@ class DataResult:
     def __str__(self):
         return f'({self.ticker})'
 
+class SymbolResult:
+    def __init__(self, symbol, company):
+        self.symbol = symbol
+        self.company = company
+
 
 
 def getNouns(title):
@@ -39,19 +45,27 @@ def get_symbol(query, preferred_exchange='AMS'):
     else:
         quotes = data['quotes']
         if len(quotes) == 0:
-            return 'No Symbol Found'
+            return SymbolResult("No Symbol Found", "")
 
-        symbol = "No Symbol Found"
+        symbol = SymbolResult("No Symbol Found", "")
         for quote in quotes:
             # print("quote = ", quote)
             try:
-                if query.lower() in quote['longname'].lower():
-                    symbol = quote['symbol']
+                symbolLike = similar(query.lower(), quote['symbol'].lower())
+                nounLike = similar(query.lower(), quote['longname'].lower())
+                print(query.lower()," = ", symbolLike)
+                if nounLike > 0.5 or symbolLike > 0.5:
+                    #if query.lower() in quote['longname'].lower():
+                    symbol = SymbolResult(str(quote['symbol']), str(quote['longname']))
                     break
             except:
                 #print(quote)
-                if query.lower() in quote['shortname'].lower():
-                    symbol = quote['symbol']
+                symbolLike = similar(query.lower(), quote['symbol'].lower())
+                nounLike = similar(query.lower(), quote['shortname'].lower())
+                print("Similar = ", symbolLike)
+                if nounLike > 0.5 or symbolLike > 0.5:
+                    #if query.lower() in quote['shortname'].lower():
+                    symbol = SymbolResult(str(quote['symbol']), str(quote['shortname']))
                     break
         return symbol
 
@@ -59,45 +73,59 @@ def getFinanceData(symbol):
     ticker = yf.Ticker(symbol)
     return ticker.history(period='max')
 
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+def cleanName(title):
+    return title.replace("/[^a-zA-Z ]/g", "").replace("|", "").replace("financial", "").replace("inc.", "").replace("co.", "")
+
 
 
 # to search
 # query = "large donations to mental health charity"
 #query = "corporate donation to healthcare media"
 
-query = "corporate donation to mental health media"
+#query = "corporate donation to mental health media"
+
+#query = "company donates to mental health"
+
+query = "news company donates to mental health"
 
 results = []
 
 for url in search(query, tld="co.in", num=10, stop=20, pause=2):
-    try:
-        # making requests instance
-        reqs = requests.get(url)
+    # try:
+    # making requests instance
+    reqs = requests.get(url)
 
-        # using the BeaitifulSoup module
-        soup = BeautifulSoup(reqs.text, 'html.parser')
+    # using the BeaitifulSoup module
+    soup = BeautifulSoup(reqs.text, 'html.parser')
 
-        # displaying the title
-        #print("Title of the website is : ")
+    # displaying the title
+    #print("Title of the website is : ")
 
-        title = soup.find('title').get_text()
+    title = soup.find('title').get_text()
 
-        print(title)
+    print(title)
 
-        title = title.replace("/[^a-zA-Z ]/g", "").replace("|", "")
+    title = cleanName(title)
 
-        nouns = getNouns(title)
+    nouns = getNouns(title)
 
-        for noun in nouns:
-            symbol = get_symbol(str(noun))
-            #print(noun, " = ", symbol)
-            if symbol != "No Symbol Found":
-                print(noun, " = ", symbol)
-                results.append(DataResult(url, noun, symbol))
-    except:
-        print("error, moving on")
+    for noun in nouns:
+        symbolResult = get_symbol(str(noun))
+        print(symbolResult.symbol)
+        #print(noun, " = ", symbol)
+        #if (symbol != "No Symbol Found") and (any(x.ticker == symbol.symbol for x in results)):
+        if (symbolResult.symbol != "No Symbol Found"):
+            print(noun, " = ", symbolResult.symbol)
+            results.append(DataResult(url, symbolResult.company, symbolResult.symbol))
+    # except:
+    #     print("error, moving on")
 
     #print("END")
+print(results)
+
 for result in results:
     print(result.url)
     print(result.company)
